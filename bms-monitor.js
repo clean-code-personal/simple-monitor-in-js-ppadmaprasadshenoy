6060702610b7e6b5619ed2048d7534ac528a3bf5
P PADMAPRASAD SHENOY <ppadmaprasadshenoy@gmail.com>
2023-03-05 22:55:06 +0530

    Changes

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,32 +1,32 @@
-function isValueOutOfRange(value, min, max, parameterName) {
-    if (value < min) {
-      console.log(`${parameterName} is too low!`);
+function inRange(val, min, max) {
+    if(val<min || val>max){
+      return false;
+    } else{
       return true;
     }
-    if (value > max) {
-      console.log(`${parameterName} is too high!`);
-      return true;
-    }
-    return false;
+  }
+  
+  function checkTemperatureRange(temperature) {
+    return inRange(temperature, 0, 45);
+  }
+  function checkSocRange(soc) {
+    return inRange(soc, 20, 80);
+  }
+  function checkChargeRateRange(charge_rate) {
+    return inRange(charge_rate, 0, 0.8);
+  }
+  
+  function check(parameters){
+    return parameters;
   }
   
   function batteryIsOk(temperature, soc, charge_rate) {
-    let isBatteryOk = true;
-  
-    if (isValueOutOfRange(temperature, 0, 45, "Temperature")) {
-      isBatteryOk = false;
-    }
-  
-    if (isValueOutOfRange(soc, 20, 80, "State of Charge")) {
-      isBatteryOk = false;
-    }
-  
-    if (isValueOutOfRange(charge_rate, 0, 0.8, "Charge rate")) {
-      isBatteryOk = false;
-    }
-  
+    let temperatureValue = checkTemperatureRange(temperature);
+    let socValue = checkSocRange(soc);
+    let chargeRateValue = checkChargeRateRange(charge_rate);
+    const parameters = [temperatureValue, socValue, chargeRateValue];
+    let isBatteryOk = parameters.every(check);
     return isBatteryOk;
   }
   
-  module.exports = {batteryIsOk};
-  
+  module.exports = { batteryIsOk };
