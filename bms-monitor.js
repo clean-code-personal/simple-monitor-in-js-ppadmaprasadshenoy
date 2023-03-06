2c3492a3ba4a49970509cfa3dc5340e37d96a16c
P PADMAPRASAD SHENOY <ppadmaprasadshenoy@gmail.com>
2023-03-06 12:28:12 +0530

    Changes

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,32 +1,52 @@
-function inRange(val, min, max) {
-  if(val<min || val>max){
-    return false;
-  } else{
-    return true;
+const MEASUREMENT_LIMITS = {
+  temperature: { limit: { min: 0, max: 45 }, tolerance: 0.05, unit: 'Celsius' },
+  soc: { limit: { min: 20, max: 80 }, tolerance: 0.05, unit: '%' },
+  charge_rate: { limit: { min: 0, max: 0.8 }, tolerance: 0.05, unit: 'per hour' }
+};
+
+function convertToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function checkValueInRange(value, limit, tolerance, unit) {
+  const limitInCelsius = {
+    min: limit.min,
+    max: limit.max
+  };
+
+  if (unit === 'Fahrenheit') {
+    limitInCelsius.min = convertToFahrenheit(limit.min);
+    limitInCelsius.max = convertToFahrenheit(limit.max);
+  }
+
+  const upperLimit = limitInCelsius.max;
+  const lowerLimit = limitInCelsius.min;
+  const upperWarningLimit = upperLimit - (upperLimit * tolerance);
+  const lowerWarningLimit = lowerLimit + (upperLimit * tolerance);
+
+  if (value < lowerLimit) {
+    return 'LOW';
+  } else if (value > upperLimit) {
+    return 'HIGH';
+  } else if (value >= lowerWarningLimit && value <= lowerLimit) {
+    return 'WARNING: Approaching discharge';
+  } else if (value >= upperLimit && value <= upperWarningLimit) {
+    return 'WARNING: Approaching charge-peak';
+  } else {
+    return 'NORMAL';
   }
 }
 
-function checkTemperatureRange(temperature) {
-  return inRange(temperature, 0, 45);
-}
-function checkSocRange(soc) {
-  return inRange(soc, 20, 80);
-}
-function checkChargeRateRange(charge_rate) {
-  return inRange(charge_rate, 0, 0.8);
-}
+function batteryIsOk(temperature, soc, charge_rate, temperatureUnit = 'Celsius') {
+  const temperatureStatus = checkValueInRange(temperature, MEASUREMENT_LIMITS.temperature.limit, MEASUREMENT_LIMITS.temperature.tolerance, temperatureUnit);
+  const socStatus = checkValueInRange(soc, MEASUREMENT_LIMITS.soc.limit, MEASUREMENT_LIMITS.soc.tolerance, MEASUREMENT_LIMITS.soc.unit);
+  const chargeRateStatus = checkValueInRange(charge_rate, MEASUREMENT_LIMITS.charge_rate.limit, MEASUREMENT_LIMITS.charge_rate.tolerance, MEASUREMENT_LIMITS.charge_rate.unit);
 
-function check(parameters){
-  return parameters;
-}
+  console.log(`Temperature is ${temperature} ${temperatureUnit}. Status: ${temperatureStatus}`);
+  console.log(`State of Charge is ${soc}${MEASUREMENT_LIMITS.soc.unit}. Status: ${socStatus}`);
+  console.log(`Charge Rate is ${charge_rate}${MEASUREMENT_LIMITS.charge_rate.unit}. Status: ${chargeRateStatus}`);
 
-function batteryIsOk(temperature, soc, charge_rate) {
-  let temperatureValue = checkTemperatureRange(temperature);
-  let socValue = checkSocRange(soc);
-  let chargeRateValue = checkChargeRateRange(charge_rate);
-  const parameters = [temperatureValue, socValue, chargeRateValue];
-  let isBatteryOk = parameters.every(check);
-  return isBatteryOk;
+  return temperatureStatus === 'NORMAL' && socStatus === 'NORMAL' && chargeRateStatus === 'NORMAL';
 }
 
 module.exports = { batteryIsOk };
